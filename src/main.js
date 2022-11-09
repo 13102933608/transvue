b858c9251ce62cdbc0933826ad37ba3f9f85ded0
JSR <729326443@qq.com>
2022-11-09 15:36:30 +0800

    添加服务器注释

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import axios from 'axios'
 Vue.prototype.$http = axios
 // 发布的时候把baseURL 的localhost改成服务器ip，只需要改这一处就行
 axios.defaults.baseURL = 'http://localhost:9000'
-// axios.defaults.baseURL = 'http://10.232.69.219:9000'
+// axios.defaults.baseURL = 'http://10.232.69.219:9000' 后台服务器
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 /* eslint-disable no-new */
